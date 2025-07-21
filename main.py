6bc23fc60ac9ccd935ef5904a2d5e76a3c7c5492
sbogachuk <150458183+sbogachuk@users.noreply.github.com>
2025-07-21 16:05:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ GROUP_ID = int(os.getenv('GROUP_ID'))  # Отримуємо з змінних с
 bot = telegram.Bot(token=TOKEN)
 
 # Час для відправки опитування
-TARGET_HOUR = 15
-TARGET_MINUTE = 25
+TARGET_HOUR = 16
+TARGET_MINUTE = 07
 
 async def send_poll_async():
     try:
