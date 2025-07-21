de69654d51bb775c107f58e27c0c98908cd56458
sbogachuk <150458183+sbogachuk@users.noreply.github.com>
2025-07-21 16:35:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import asyncio
 import logging
-from telegram import Bot
 from telegram.ext import Application
 import pytz
 import datetime
 import os
 
-# Базове логування
-logging.basicConfig(level=logging.INFO)
-
-TOKEN = os.getenv("TELEGRAM_TOKEN", "тут_токен_бота")
+TOKEN = os.getenv("TELEGRAM_TOKEN", "7642593461:AAHb_XPxyReVjL42PRTZoyoUioQzuv_Ym3c")
 GROUP_ID = int(os.getenv("GROUP_ID", "-4923200694"))
 
-# Час для надсилання опитування
 TARGET_HOUR = 16
-TARGET_MINUTE = 33
+TARGET_MINUTE = 38
 
-async def send_poll_async(application: Application):
-    """Функція надсилання опитування"""
+logging.basicConfig(level=logging.INFO)
+
+async def send_poll_async(application):
     try:
         await application.bot.send_poll(
             chat_id=GROUP_ID,
@@ -32,12 +28,11 @@ async def send_poll_async(application: Application):
         logging.error(f"Помилка при відправці опитування: {e}")
         return False
 
-async def scheduler(application: Application):
+async def scheduler(application):
     while True:
         now = datetime.datetime.now(pytz.timezone('Europe/Kyiv'))
         target_time = now.replace(hour=TARGET_HOUR, minute=TARGET_MINUTE, second=0, microsecond=0)
         if now > target_time:
-            # Якщо час уже пройшов — чекаємо до завтра
             target_time += datetime.timedelta(days=1)
         delay = (target_time - now).total_seconds()
         logging.info(f"Чекаємо {delay} секунд до {target_time}")
@@ -46,11 +41,8 @@ async def scheduler(application: Application):
 
 async def main():
     application = Application.builder().token(TOKEN).build()
-    
-    # Запуск асинхронного планувальника
+    # scheduler запускається в окремому таску
     asyncio.create_task(scheduler(application))
-    
-    # Запуск бота (відкриває polling)
     await application.run_polling()
 
 if __name__ == '__main__':
